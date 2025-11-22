fd8f8cf4806d1686dc91cb18dd5a375596090ed3
azuhra2197 <azuhra2197@gmail.com>
2025-11-22 17:24:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+def fasl_aniqla(oy):
+    if oy == 12 or oy == 1 or oy == 2:
+        print("Qish")
+    elif oy == 3 or oy == 4 or oy == 5:
+        print("Bahor")
+    elif oy == 6 or oy == 7 or oy == 8:
+        print("Yoz")
+    elif oy == 9 or oy == 10 or oy == 11:
+        print("Kuz")
+    else:
+        print("Bu oyda fasl yo'q")
+
+
+fasl_aniqla(13)
+
+fasl_aniqla(5)
+
+fasl_aniqla(7)
+
+fasl_aniqla(1)
+
+fasl_aniqla(3)
